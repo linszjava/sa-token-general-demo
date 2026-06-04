6551dd9bc0c7b58dca42412b917070a0b0405571
linsz <linszjava@gmail.com>
2026-06-05 00:46:44 +0800

    SaSession 会话 通过

--- sa-token-springboot3-demo/src/main/java/com/size/springboot3/exception/HandleException.java
+++ sa-token-springboot3-demo/src/main/java/com/size/springboot3/exception/HandleException.java
@@ -56,6 +56,7 @@ public class HandleException {
     @ExceptionHandler(Exception.class)
     public R<Void> handleException(Exception e) {
         log.error("系统异常", e);
+        // 默认如下
         //return R.fail(ResultCode.FAIL.getCode(), ResultCode.FAIL.getMsg());
         return R.fail(ResultCode.FAIL.getCode(), ResultCode.FAIL.getMsg()+":"+e.getMessage());
     }
